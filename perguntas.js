92b940cecd76a3bdc531bdd1bc326a90f3210853
Macarrão Iguana <160538079+taina-golemba@users.noreply.github.com>
2024-11-04 16:30:32 -0300

    uy

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Saúde',
     'O que é pneumoultramicroscopicossilicovulcanoconiótico?',
-    'Descreve o indivíduo que possui doença pulmonar causada pela inspiração de cinzas vulcânicas'
+    'Descreve o indivíduo que possui doença pulmonar causada pela inspiração de cinzas vulcânicas',
 )
 
 criaCartao(
     'Dica',
-    'Se você está se sentindo sozinho, abandonado, achando que ninguém liga para você...'
-    'Atrase um pagamento'
+    'Se você está se sentindo sozinho, abandonado, achando que ninguém liga para você...',
+    'Atrase um pagamento',
 )
 
 criaCartao(
     'Piada',
     'O que a Lua disse ao Sol?',
-    '"Nossa, você é tão grande e ainda não te deixam sair à noite!"'
+    '"Nossa, você é tão grande e ainda não te deixam sair à noite!"',
 )
 
 criaCartao(
     'Biologia',
-    'Quais são os parentes mais próximos dos dinossauros atualmente?'
-    'As galinhas'
+    'Quais são os parentes mais próximos dos dinossauros atualmente?',
+    'As galinhas',
 )
